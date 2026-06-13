32e7dbfe706c093ee162f7646eb398c93411700e
sekisei0077 <sekisei.0077@gmail.com>
2026-06-14 00:28:36 +0900

    mod init

---
+++ src/client/java/dev/acecore/commonmodal/client/CommonmodalClient.java
@@ -0,0 +1,10 @@
+package dev.acecore.commonmodal.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CommonmodalClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/dev/acecore/commonmodal/Commonmodal.java
@@ -0,0 +1,10 @@
+package dev.acecore.commonmodal;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Commonmodal implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
